252ad58805f05d29f6cd44c6ee037edb0fa69ed6
malan88 <m@stdwtr.io>
2021-02-17 09:35:33 -0500

    Add more columns, better relationships

--- models.py
+++ models.py
@@ -20,15 +20,36 @@ Base = declarative_base(cls=Base_)
 
 
 class Series(Base):
-    kind = Column(String)
-    series = Column(Text)
-    correlations = relationship("Correlation", backref="series")
+    kind = Column(String)       # this will have to be a type system we create
+    series = Column(Text)       # a json object of timeseries data
+    first = Column(Date)        # the first date in the series
+    last = Column(Date)         # the last date in the series
 
+    @property
+    def correlations(self):
+        return self.corr_a + self.corr_b
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return f"<Series {self.kind} {self.first} {self.last}>"
 
 class Correlation(Base):
     series_a = Column(Integer, ForeignKey(Series.id))
     series_b = Column(Integer, ForeignKey(Series.id))
-    score = Column(Float)
+    score = Column(Float)       # pearson * lag
+    pearson = Column(Float)
+    lag = Column(Integer)       # positive a leads, negative b leads
+
+    a = relationship("Series", backref="corr_a", foreign_keys=[series_a])
+    b = relationship("Series", backref="corr_b", foreign_keys=[series_b])
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return f"<Correlation {self.a.kind} {self.b.kind} {self.score}>"
 
 
 def get_session():
